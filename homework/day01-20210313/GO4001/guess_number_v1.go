82b53d0f4482ed91b1c1e95ef9352942cf53a8e2
wang dou <kuowang@kuodeiMac.local>
2021-03-14 12:05:40 +0800

    push hkday1

---
+++ homework/day01-20210313/GO4001/guess_number_v1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+//生成一个随机数让用户猜，提示猜大猜小，最多5次哦
+func main() {
+	var i int //i定义次数
+
+	//random生成随机数
+	rand.Seed(time.Now().UnixNano())
+	var target = rand.Intn(101)
+	for i = 1; i <= 5; i++ {
+		var input int
+		fmt.Printf("请输入一个1-100的数字，当前是第%d次猜:", i)
+		fmt.Scan(&input)
+		switch {
+		case input < target:
+			fmt.Printf("猜小了,你还有%d次机会\n", 5-i)
+		case input > target:
+			fmt.Printf("猜大了,你还有%d次机会\n", 5-i)
+		case input == target:
+			fmt.Printf("猜对了,你猜了%d次\n", i)
+		default:
+			break
+		}
+
+	}
+	fmt.Printf("你没机会了，正确答案是%d\n", target)
+}
