8afc1d99fe1c4403556b10eb8c9e0a3212a69a5a
Mr.Yang <xiaowo_106@126.com>
2021-03-15 11:21:07 +0800

    杨承佳作业提交

--- homework/day01-20210313/GO4045-yangchengjia/99乘法表.go
+++ homework/day01-20210313/GO4045-yangchengjia/99乘法表.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	for i := 1; i < 10; i++ {
 		for j := 1; j <= i; j++ {
-			fmt.Printf("%d*%d=%d  ", i, j, i*j)
+			fmt.Printf("%d*%d=%d\t", i, j, i*j)
 		}
 		fmt.Println()
 	}
