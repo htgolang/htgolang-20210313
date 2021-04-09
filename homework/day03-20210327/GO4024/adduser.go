1f5648a5719b524579bcd8d3e1c38c2e67042317
Water <pfang7878@gmail.com>
2021-04-09 21:24:19 +0800

    update homework
    Signed-off-by: Water <pfang7878@gmail.com>

---
+++ homework/day03-20210327/GO4024/adduser.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var users = []map[string]string {
+	{
+		"id":"1",
+		"name":"arun",
+		"addr": "Beijing",
+		"tel": "xxx123123123",
+	},
+	{
+		"id" : "2",
+		"name": "ujianfeng",
+		"addr":"Beijing",
+		"tel":"152xxxxxxx",
+	},
+}
+
+func genId() int{
+	id := 0
+	for _,user := range users {
+		i, _ := strconv.Atoi(user["id"])
+		if i > id {
+			id = i
+		}
+	}
+	return id + 1
+}
+
+func input(prompt string) string {
+	fmt.Println(prompt)
+	var text string
+	fmt.Scan(&text)
+	return text
+}
+
+func adduser() error {
+	name := input("请输入用户名:")
+	addr := input("请输入地址:")
+	tel := input("请输入电话:")
+
+	//验证用户名不能重复
+
+	if name == "kk" {
+		return fmt.Errorf("用户名%s已存在",name)
+	}
+	users = append(users,map[string]string{
+		"id": strconv.Itoa(genId()),
+		"name" : name,
+		"addr" : addr,
+		"tel" : tel,
+	})
+	return nil 
+}
+
+func main() {
+
+	// var name, tel, addr string
+	// fmt.Println("请输入用户名:")
+	// fmt.Scan(&name)
+	// fmt.Println("请输入地址:")
+	// fmt.Scan(&addr)
+	// fmt.Println("请输入电话:")
+	// fmt.Scan(&tel)
+	if err := adduser(); err == nil {
+		fmt.Println(users)
+	} else {
+		fmt.Println("添加失败",err)
+	}
+	
+}
---
+++ homework/day03-20210327/GO4024/deluser.go
@@ -0,0 +1,64 @@
+package main
+// 为什么有index out of range这个错误
+import (
+	"fmt"
+	"strconv"
+)
+
+var users = []map[string]string {
+	{
+		"id":"1",
+		"name":"arun",
+		"addr": "Beijing",
+		"tel": "xxx123123123",
+	},
+	{
+		"id" : "2",
+		"name": "ujianfeng",
+		"addr":"Beijing",
+		"tel":"152xxxxxxx",
+	},
+	{
+		"id" : "3",
+		"name" : "John",
+		"addr" : "Shanghai",
+		"tel" : "132xxxxx",
+	},
+}
+
+func deluser() []map[string]string{
+	id := 0
+	fmt.Println("请输入要删除的用户ID:")
+	fmt.Scan(&id)
+	for i, _ := range users{
+		if users[i]["id"] == strconv.Itoa(id) {
+			// fmt.Println("你想删除用户:",users[k])
+			fmt.Println("===========用户信息如下：=============")
+			for k,v := range users[i] {
+				fmt.Println(k,v)
+			}
+			fmt.Println("==========================")
+			
+			var confirm string
+			fmt.Println("你确认要删除这个用户？(y/yes/Y/YES)")
+			fmt.Scan(&confirm)
+			if confirm == "y" || confirm =="yes" || confirm =="Y" || confirm =="YES" {
+				if i != len(users){
+					users = append(users[:i],users[i+1:]...)
+				} else {
+					users = users[:len(users)-1]
+				}
+			
+			//users = users[:len(users)-1]
+			}
+	}
+}
+	return users
+}
+
+func main() {
+	
+	fmt.Println(deluser())
+	
+
+}
