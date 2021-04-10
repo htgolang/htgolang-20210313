ef71e772c2f7dad6121dfc14b57c3e7b927f7fff
wuchang0201 <77275773+wuchang0201@users.noreply.github.com>
2021-04-10 17:25:36 +0800

    Update UserManage.go

--- homework/day03-20210327/GO4017-邬昌/UserManage.go
+++ homework/day03-20210327/GO4017-邬昌/UserManage.go
@@ -5,14 +5,159 @@ import (
 	"strconv"
 )
 
+
+var id int
+var name, addr,tel string
+
+var users = []map[string]string{
+	{
+		"id"  : "1",
+		"name": "小明",
+		"addr": "北京",
+		"tel" : "18079220200",
+	},
+}
+func getId() int  {
+
+	for _,value :=range users{
+		i,_ := strconv.Atoi(value["id"])
+		fmt.Println("i的值为：",i)
+		if i > id{
+			id =i
+		}}
+	return id+1
+}
+//添加用户
+
+func input(str string) string  {
+
+	var text string
+	fmt.Println(str)
+	fmt.Scan(&text)
+	return text
+
+}
+func addUser()([]map[string]string)  {
+
+	name :=input("请输入用户名：")
+	addr :=input("请输入地址：")
+	tel :=input("请输入电话号码：")
+
+	for _,value :=range users{
+		if value["name"] ==name{
+
+			fmt.Println("用户%s已存在",name)
+			return users
+		}
+
+	}
+
+	users =append(users, map[string]string{
+			"id": strconv.Itoa(getId()),
+			"name": name,
+			"addr": addr,
+			"tel":  tel,
+	})
+
+	return users
+}
+
+//删除用户
+func deletUser()[]map[string]string {
+
+	delid :=input("请输入要删除的用户id：")
+
+	//遍历users查看是否存在key
+	for _,value :=range users{
+		fmt.Println("value",value["id"])
+		if delid ==value["id"]{
+			fmt.Println("用户信息为：",users)
+
+			str :=input("请输入y/yes/Y/YES确认删除:")
+
+			if (str =="y" || str =="yes" || str =="Y" || str =="YES" ){
+
+				i,_:=strconv.Atoi(delid)
+				users = append(users[:i-1], users[i:]...)
+				fmt.Println("用户",delid,"被删除了！！！")
+				return users
+			}
+		}else {
+			    fmt.Println("查无此人")
+		}
+	}
+
+	return nil
+}
+
+//查询用户
+func queryUsers()[]map[string]string{
+
+
+	querystr :=input("请输入要查询的用户名:")
+	//判断map中是否存在输入的元素
+	for _,value :=range users{
+		if value["name"] ==querystr{
+
+			return users
+		}else {
+
+			fmt.Println("查无此人")
+		}
+	}
+
+	return nil
+}
+
+//修改用户
+func modifyUsers() []map[string]string{
+
+	modifyid :=input("请输入要修改的用户id：")
+
+	for _,value :=range users{
+		if modifyid ==value["id"]{
+			fmt.Println("用户信息为：",users)
+			str :=input("请输入y/yes/Y/YES确认修改:")
+
+			if (str =="y" || str =="yes" || str =="Y" || str =="YES" ){
+				name :=input("请输入用户名：")
+
+			    addr :=input("请输入地址：")
+
+				tel :=input("请输入电话号码；")
+
+				users =append(users, map[string]string{
+					"id": modifyid,
+					"name": name,
+					"addr": addr,
+					"tel":  tel,
+				})
+
+				return users
+
+			}else{
+				break
+			}
+
+		}else {
+
+			fmt.Println("查无此人")
+
+		}
+	}
+
+	return nil
+}
+
 func main() {
 
 	var do int
+
 	/*
-	执行操作：
-	1、添加用户  2、删除用户
-	3、查询用户  4、修改用户
-	 */
+		执行操作：
+		1、添加用户  2、删除用户
+		3、查询用户  4、修改用户
+	*/
 	for {
 		fmt.Println("请输入要执行的操作,1:添加用户,2:删除用户,3:查询用户,4:修改用户,其他按键退出")
 		fmt.Scan(&do)
@@ -36,117 +181,4 @@ func main() {
 		}
 	}
 
-}
-//添加用户
-func addUser()(map[string]map[string]string)  {
-
-	var name, addr,tel string
-	var users = make(map[string]map[string]string)
-	var id int
-	fmt.Println("请输入用户名：")
-	fmt.Scan(&name)
-
-	fmt.Println("请输入家庭地址：")
-	fmt.Scan(&addr)
-
-	fmt.Println("请输入电话号码：")
-	fmt.Scan(&tel)
-	//定义id自增
-	id++
-	users[strconv.Itoa(id)] = map[string]string{
-		"name": name,
-		"addr": addr,
-		"tel":  tel,
-	}
-
-	return users
-}
-
-//删除用户
-func deletUser()map[string]map[string]string {
-	var users map[string]map[string]string
-	var delid int
-	var str string
-	fmt.Println("请输入要删除的用户id：")
-	fmt.Scan(&delid)
-
-	//遍历users查看是否存在key
-	for key,_ :=range users{
-		if strconv.Itoa(delid) ==key{
-			fmt.Println("用户信息为：",users)
-			fmt.Println("请输入y/yes/Y/YES确认删除:")
-			fmt.Scan(&str)
-			if (str =="y" || str =="yes" || str =="Y" || str =="YES" ){
-
-				delete(users, strconv.Itoa(delid))
-				fmt.Println("用户",delid,"被删除了！！！")
-				return users
-			}
-		}else {
-			    fmt.Println("查无此人")
-		}
-	}
-
-	return nil
-}
-
-//查询用户
-func queryUsers()map[string]map[string]string{
-
-	var users map[string]map[string]string
-	var querystr string
-	fmt.Println("请输入要查询的用户名或者地址或者电话")
-	fmt.Scan(&querystr)
-	//判断map中是否存在输入的元素
-	for _,value :=range users{
-		for _,val :=range value{
-			if val ==querystr{
-				return users
-			}
-
-		}
-	}
-
-	return nil
-}
-
-//修改用户
-func modifyUsers() map[string]map[string]string{
-
-	var users map[string]map[string]string
-	var modifyid int
-	var str string
-	var name,addr,tel string
-	fmt.Println("请输入要删除的用户id：")
-	fmt.Scan(&modifyid)
-	for key,_ :=range users{
-		if strconv.Itoa(modifyid) ==key{
-			fmt.Println("用户信息为：",users)
-			fmt.Println("请输入y/yes/Y/YES确认修改:")
-			fmt.Scan(&str)
-			if (str =="y" || str =="yes" || str =="Y" || str =="YES" ){
-				fmt.Println("请输入用户名：")
-				fmt.Scan(&name)
-
-				fmt.Println("请输入地址：")
-				fmt.Scan(&addr)
-
-				fmt.Println("请输入电话号码；")
-				fmt.Scan(&tel)
-				users[strconv.Itoa(modifyid)] = map[string]string{
-					"name": name,
-					"addr": addr,
-					"tel":  tel,
-				}
-
-				return users
-			}else{
-				break
-			}
-		}else {
-			fmt.Println("查无此人")
-		}
-	}
-
-	return nil
 }
